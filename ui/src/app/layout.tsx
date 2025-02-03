1fe513f257e61d1ee6dde6d6978b17025164b70e
jxtngx <26209687+jxtngx@users.noreply.github.com>
2025-02-03 09:30:31 -0500

    refactor layout and home page for improved structure and styling

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/globals.css";
 import type { Metadata } from "next";
 import Link from "next/link";
-import { ExternalLink } from "lucide-react";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import {
@@ -46,56 +45,40 @@ export default function RootLayout({
           className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <nav className="flex items-center justify-between mt-8 ml-12 mr-12 mb-4">
+            <nav className="flex items-center justify-between mt-8 ml-4 mr-4 mb-4 sm:ml-12 sm:mr-12">
               <div>
-                <ModeSwitcher />
+              <ModeSwitcher />
               </div>
               <div className="flex flex-1 justify-left gap-6 ml-4">
-                <Link href="/">Home</Link>
-                <SignedIn>
-                  <Link className="items-center flex gap-1" href="/lab">
-                    Tune Lab
-                  </Link>
-                </SignedIn>
-                <SignedOut>
-                  <Link
-                    className="items-center flex gap-1"
-                    href="https://github.com/pytorch/torchtune"
-                    target="_blank"
-                  >
-                    {`torchtune`} <ExternalLink height="12" width="12" />
-                  </Link>
-                  <Link
-                    className="items-center flex gap-1"
-                    href="https://github.com/pytorch/ao"
-                    target="_blank"
-                  >
-                    {`torchao`} <ExternalLink height="12" width="12" />
-                  </Link>
-                </SignedOut>
+              <Link href="/">Home</Link>
+              <SignedIn>
+                <Link className="items-center flex gap-1" href="/lab">
+                Tune Lab
+                </Link>
+              </SignedIn>
               </div>
               <div className="flex flex-1 justify-end">
-                <SignedIn>
-                  <UserButton />
-                </SignedIn>
-                <SignedOut>
-                  <SignInButton>
-                    <Link
-                      href="/sign-in"
-                      className="btn flex gap-2 justify-center items-center"
-                    >
-                      Sign In <LogIn height="18" width="18" />
-                    </Link>
-                  </SignInButton>
-                </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              <SignedOut>
+                <SignInButton>
+                <Link
+                  href="/sign-in"
+                  className="btn flex gap-2 justify-center items-center"
+                >
+                  Sign In <LogIn height="18" width="18" />
+                </Link>
+                </SignInButton>
+              </SignedOut>
               </div>
             </nav>
             {children}
-            <footer className="mb-16">
+            <footer className="mt-auto">
               <SignedOut>
-                <p className="text-neutral-600 dark:text-neutral-300 items-center flex flex-col gap-1 text-xs">
-                  © {new Date().getFullYear()} Theosis AI
-                </p>
+              <p className="text-neutral-600 dark:text-neutral-300 items-center flex flex-col gap-1 text-xs">
+                © {new Date().getFullYear()} Theosis AI
+              </p>
               </SignedOut>
             </footer>
           </ThemeProvider>
