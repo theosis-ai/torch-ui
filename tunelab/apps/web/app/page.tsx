59b9adc51168c88334f38beb0c844b5264d3efaf
jxtngx <26209687+jxtngx@users.noreply.github.com>
2025-02-12 10:57:57 -0500

    fix: improve homepage description for clarity

--- tunelab/apps/web/app/page.tsx
+++ tunelab/apps/web/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <Icons.gitHub className="h-8 w-8 mb-10" />
       </Link>
       <h1 className="text-4xl font-medium tracking-wider mb-2">Tune Lab</h1>
-      <p className="font-sans tracking-wider mb-12">A Next.js App for Meta's TorchTune</p>
+      <p className="font-sans tracking-wider mb-12">A Next.js application for Meta's TorchTune</p>
     </div>
   );
 }
