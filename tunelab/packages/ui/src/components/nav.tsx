48c00210998ad5ae1827598bf43775308aa940a6
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2025-02-17 19:11:58 -0500

    update configs and globals

--- tunelab/packages/ui/src/components/nav.tsx
+++ tunelab/packages/ui/src/components/nav.tsx
@@ -1,23 +1,41 @@
 "use client";
 
-import Link from "next/link";
 import { LogIn } from "lucide-react";
-import ModeSwitcher from "@workspace/ui/components/modeSwitcher";
+import { ModeSwitcher } from "@workspace/ui/components/modeSwitcher";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@workspace/ui/components/navigation-menu";
 
 export default function Nav() {
   return (
-    <nav className="flex items-center justify-between mt-8 ml-4 mr-4 mb-4 sm:ml-12 sm:mr-12">
-      <div>
-        <ModeSwitcher />
-      </div>
-      <div className="flex flex-1 justify-end">
-        <Link
-          href="/lab"
-          className="btn flex gap-2 justify-center items-center"
-        >
-          Lab <LogIn height="18" width="18" />
-        </Link>
-      </div>
+    <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="container-wrapper">
+      <div className="container flex h-14 items-center">
+      <NavigationMenu className="flex items-center gap-4 text-sm xl:gap-6">
+      
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/lab">
+            Lab
+            <LogIn height="18" width="18" />
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+    {/* mode switcher */}
+    <nav className="flex items-center gap-0.5">
+    <div className="flex flex-1 items-center justify-between gap-2 md:justify-end">
+      <ModeSwitcher />
+    </div>
     </nav>
+    
+
+    
+    </div>
+      </div>
+    </header>
   );
 }
